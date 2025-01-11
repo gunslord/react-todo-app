11f4600e9bf15aaa2f1b70b5724709257b36f108
gunslord <gunslord@gmail.com>
2025-01-11 14:15:25 +0900

    프로젝트 완성

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import TodoHeader from "./components/TodoHeader";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+function fetchTodos() {
+  const result = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const value = localStorage.key(i);
+    result.push(value);
+  }
+  return result;
+}
+
+function App() {
+  const [todos, setTodos] = useState(fetchTodos());
+
+  const addTodo = (todo) => {
+    localStorage.setItem(todo, todo);
+
+    setTodos((currentTodos) => {
+      return [...currentTodos, todo];
+    });
+  };
+
+  const removeTodo = (todo) => {
+    const result = todos.filter((todoItem) => {
+      if (todoItem !== todo) {
+        return true;
+      }
+    });
+    setTodos(result);
+    localStorage.removeItem(todo);
+  };
+
+  return (
+    <div>
+      <TodoHeader></TodoHeader>
+      <TodoInput onTodoAdd={addTodo}></TodoInput>
+      <TodoList todos={todos} onTodoRemove={removeTodo}></TodoList>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoHeader.jsx
@@ -0,0 +1,5 @@
+function TodoHeader() {
+  return <h1>TOTO 앱</h1>;
+}
+
+export default TodoHeader;
